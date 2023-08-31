10eabac0b9afec08a9c70aebd76ae2705ed398ee
khawla <boukniter.khawla@gmail.com>
2023-08-31 12:23:46 -0700

    ..

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -23,7 +23,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		n->left = parent->left;
 		parent->left->parent = n;
 	}
-	parent->left = new;
+	parent->left = n;
 
-	return (new);
+	return (n);
 }
